981fea64d35a2227adeadd7af3c5e3c07d4a6d62
KOKKILIGADDA SNEHA DURGA <144988647+kokkiligaddasnehadurga@users.noreply.github.com>
2024-07-12 19:12:40 +0530

    Create write a program to find greatest of three numbers.py

---
+++ write a program to find greatest of three numbers.py
@@ -0,0 +1,9 @@
+n1=int(input())
+n2=int(input())
+n3=int(input())
+if ((n1>n2) & (n1>n3)):
+    print("n1 largest ")
+elif ((n2>n1)&(n2>n3)):
+    print("n2 largest")
+else:
+    print ("n3 largest")
